0cba8d90e025f83c49e6b045c90036b040bc2017
Arpit Khandelwal <68700864+Arpit-Khandelwal@users.noreply.github.com>
2026-05-17 22:16:57 +0530

    feat: add Microsoft Clarity (wslz4bfzte)

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,6 +26,13 @@ export default function RootLayout({
           />
         </noscript>
         {children}
+        <Script id="clarity" strategy="afterInteractive">{`
+(function(c,l,a,r,i,t,y){
+c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+})(window,document,"clarity","script","wslz4bfzte");
+        `}</Script>
         <Script id="gtm" strategy="afterInteractive">{`
 (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
