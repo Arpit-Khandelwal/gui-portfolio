f4d368911c4f3f9c859c88a9cec77d134578ff3d
Arpit Khandelwal <68700864+Arpit-Khandelwal@users.noreply.github.com>
2026-05-17 22:13:52 +0530

    feat: add Google Tag Manager (GTM-K2XHHRXL)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -15,7 +16,24 @@ export default function RootLayout({
 {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-K2XHHRXL"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {children}
+        <Script id="gtm" strategy="afterInteractive">{`
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-K2XHHRXL');
+        `}</Script>
+      </body>
     </html>
   );
 }
